d10c17de675637699b091782041b8c2ea992e3a6
edwinchen85 <edwinchen85@gmail.com>
2017-12-12 03:53:36 +0800

    Rendering dynamic markers

--- src/js/App.js
+++ src/js/App.js
@@ -14,7 +14,15 @@ class App extends React.Component {
       activeProperty: data.properties[0]
     }
 
+    this.setActiveProperty = this.setActiveProperty.bind(this);
   }
+
+  setActiveProperty(property) {
+    this.setState({
+      activeProperty: property
+    });
+  }
+
   render(){
     const { properties, activeProperty } = this.state;
     return (
@@ -113,7 +121,10 @@ class App extends React.Component {
         </div>
         {/* listings - End */}
 
-        <GoogleMap properties={properties} activeProperty={activeProperty} />
+        <GoogleMap
+          properties={properties} activeProperty={activeProperty}
+          setActiveProperty={this.setActiveProperty}
+        />
 
       </div>
     )
--- src/js/GoogleMap.js
+++ src/js/GoogleMap.js
@@ -12,13 +12,47 @@ class GoogleMap extends React.Component {
   }
 
   componentDidMount() {
-    const { activeProperty } = this.props;
+    const { properties, activeProperty } = this.props;
     const { latitude, longitude } = activeProperty;
 
     this.map = new google.maps.Map(this.refs.map, {
       center: {lat: latitude, lng: longitude},
       mapTypeControl: false,
       zoom: 15
+    });
+
+    this.createMarkers(properties);
+  }
+
+  createMarkers(properties) {
+    const { setActiveProperty } = this.props;
+
+    properties.map(property => {
+      const { latitude, longitude, index } = property;
+      this.marker = new google.maps.Marker({
+        position: {lat: latitude, lng: longitude},
+        map: this.map,
+        label: {
+          color: '#ffffff',
+          text: `${index+1}`
+        },
+        icon: {
+          url: "https://ihatetomatoes.net/react-tutorials/google-maps/images/img_map-marker.png",
+          // This marker is 22 pixels wide by 55 pixels high.
+          size: new google.maps.Size(22, 55),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, -15),
+          // The anchor for this image is the base of the cross (11, 52).
+          anchor: new google.maps.Point(11, 52)
+        }
+      });
+
+      this.marker.addListener('click', function() {
+
+        // set active property into the state
+        setActiveProperty(property);
+
+      });
     });
   }
 
@@ -34,7 +68,8 @@ class GoogleMap extends React.Component {
 
 GoogleMap.propTypes = {
   properties: PropTypes.array.isRequired,
-  activeProperty: PropTypes.object.isRequired
+  activeProperty: PropTypes.object.isRequired,
+  setActiveProperty: PropTypes.func.isRequired
 };
 
 export default GoogleMap;
