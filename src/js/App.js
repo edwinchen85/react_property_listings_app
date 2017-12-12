cf9365bd2b36737b2a55c8bece153c27a1443958
Edwin Chen <edwinchen85@gmail.com>
2017-12-12 19:52:08 +0800

    Header and Filter components

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import image from '../images/house-location-pin.svg'
 import Card from './Card';
+import Header from './Header';
 import GoogleMap from './GoogleMap';
 import data from './data/Data';
 import jump from 'jump.js';
@@ -13,10 +13,19 @@ class App extends React.Component {
 
     this.state = {
       properties: data.properties,
-      activeProperty: data.properties[0]
+      activeProperty: data.properties[0],
+      filterIsVisible: false
     }
 
     this.setActiveProperty = this.setActiveProperty.bind(this);
+    this.toggleFilter = this.toggleFilter.bind(this);
+  }
+
+  toggleFilter(e) {
+    e.preventDefault();
+    this.setState({
+      filterIsVisible: !this.state.filterIsVisible
+    })
   }
 
   setActiveProperty(property, scroll) {
@@ -39,86 +48,16 @@ class App extends React.Component {
   }
 
   render(){
-    const { properties, activeProperty } = this.state;
+    const { properties, activeProperty, filterIsVisible } = this.state;
     return (
       <div>
         {/* listings - Start */}
         <div className="listings">
 
-          {/* Header - Start - add .filter-is-visible to show filter*/}
-          <header className="">
-
-            {/* Filter - Start */}
-            <form className="filter">
-                <div className="filterBox">
-                    <label htmlFor="filterBedrooms">Bedrooms</label>
-                    <select id="filterBedrooms" name="filterBedrooms">
-                        <option value="any">Any</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                    </select>
-                </div>
-                <div className="filterBox">
-                    <label htmlFor="filterBathrooms">Bathrooms</label>
-                    <select id="filterBathrooms" name="filterBathrooms">
-                        <option value="any">Any</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                    </select>
-                </div>
-                <div className="filterBox">
-                    <label htmlFor="filterCars">Car Spaces</label>
-                    <select id="filterCars" name="filterCars">
-                        <option value="any">Any</option>
-                        <option value="0">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                    </select>
-                </div>
-                <div className="filterBox filterFrom">
-                    <label htmlFor="priceFrom">Min Price</label>
-                    <select id="priceFrom" name="priceFrom">
-                        <option value="0">Any</option>
-                        <option value="500000">{500000}</option>
-                        <option value="600000">{600000}</option>
-                        <option value="700000">{700000}</option>
-                        <option value="800000">{800000}</option>
-                        <option value="900000">{900000}</option>
-                    </select>
-                </div>
-                <div className="filterBox">
-                    <label htmlFor="priceTo">Max Price</label>
-                    <select id="priceTo" name="priceTo">
-                        <option value="1000001">Any</option>
-                        <option value="600000">{600000}</option>
-                        <option value="700000">{700000}</option>
-                        <option value="800000">{800000}</option>
-                        <option value="900000">{900000}</option>
-                        <option value="1000000">{1000000}</option>
-                    </select>
-                </div>
-                <div className="filterBox">
-                    <label htmlFor="filterSort">Order by</label>
-                    <select id="filterSort" name="filterSort">
-                        <option value="any">Default</option>
-                        <option value="0">Price: - Low to High</option>
-                        <option value="1">Price: - High to Low</option>
-                    </select>
-                </div>
-                <div className="filterBox">
-                    <label>&nbsp;</label>
-                    <button className="btn-clear">Clear</button>
-                </div>
-                <button className="btn-filter"><strong>X</strong><span>Close</span></button>
-            </form>
-            {/* Filter - End */}
-
-            <img src={image} />
-            <h1>Property Listings</h1>
-            <button className="btn-filter">Filter</button>
-          </header>
-          {/* Header - End */}
+          <Header
+            filterIsVisible={filterIsVisible}
+            toggleFilter={this.toggleFilter}
+          />
 
           <div className="cards container">
             <div className="cards-list row ">
